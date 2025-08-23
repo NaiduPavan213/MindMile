fb965f96f9d4ad09569970efe38e0a950ec7d072
Pavan <facts9506@gmail.com>
2025-08-23 10:45:00 +0530

    server.js file is added

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.log(`MongoDB connection error : ${err.message}`);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+app.get('/',(req,res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT , () => {
+    console.log(`server is running on port ${PORT}`);
+});
