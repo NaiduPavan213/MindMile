6b07bf868d56c330a638ca89c85958707083115d
Pavan <facts9506@gmail.com>
2025-08-23 11:30:24 +0530

    JWT_SECRET added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
